5799c567579644fe9ada53af636d10e70aff43c6
Laura Martin <surminus@gmail.com>
2016-10-30 10:56:17 +0000

    add apt source for google chrome install

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -14,11 +14,25 @@ class packages {
     'terminator',
     'wget',
     'xclip',
+    'vim',
     'zsh',
   ]
 
-  package {
-    $software: ensure => 'installed'
+  package { $software:
+    ensure  => 'installed',
+    require => Class[Apt],
+  }
+
+  include apt
+
+  apt::source { 'google-chrome':
+    location => 'http://dl.google.com/linux/chrome/deb/',
+    release  => 'stable',
+    repos    => 'main',
+    key      => {
+      'id'     => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+      'server' => 'dl-ssl.google.com',
+    },
   }
 
 }
